089c1218993e35e042fba7b0c58a611c7903374d
Dmytro Bartash <dmytro.bartash@binary-studio.com>
2026-07-26 12:22:11 +0300

    refactor: sonarqube code smell

--- ArbiScannerAdminPanel.Infrastructure/DbContext/AdminPanelAppDbContext.cs
+++ ArbiScannerAdminPanel.Infrastructure/DbContext/AdminPanelAppDbContext.cs
@@ -16,80 +16,80 @@ namespace ArbiScannerAdminPanel.Infrastructure.DbContext
         public DbSet<UserSubscriptionPayment> UserSubscriptionPayments { get; set; }
         public DbSet<AdminRefreshTokenModel> RefreshTokens { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
 
-            modelBuilder.Entity<PaymentModel>()
+            builder.Entity<PaymentModel>()
                 .HasKey(p => p.Id);
 
-            modelBuilder.Entity<PaymentModel>()
+            builder.Entity<PaymentModel>()
                 .Property(p => p.UserId)
                 .IsRequired();
 
-            modelBuilder.Entity<PaymentModel>()
+            builder.Entity<PaymentModel>()
                 .HasIndex(p => p.UserId)
                 .HasDatabaseName("IX_Payments_UserId");
 
-            modelBuilder.Entity<PaymentModel>()
+            builder.Entity<PaymentModel>()
                 .HasIndex(p => p.TransactionId)
                 .HasDatabaseName("IX_Payments_TransactionId");
 
-            modelBuilder.Entity<SubscriptionModel>()
+            builder.Entity<SubscriptionModel>()
                 .HasKey(s => s.Id);
 
-            modelBuilder.Entity<UserSubscriptionModel>()
+            builder.Entity<UserSubscriptionModel>()
                 .HasKey(u => u.Id);
 
-            modelBuilder.Entity<UserSubscriptionModel>()
+            builder.Entity<UserSubscriptionModel>()
                 .Property(u => u.UserId)
                 .IsRequired();
 
-            modelBuilder.Entity<UserSubscriptionModel>()
+            builder.Entity<UserSubscriptionModel>()
                 .HasIndex(u => new { u.UserId, u.EndDate })
                 .HasDatabaseName("IX_UserSubscriptions_UserId_EndDate");
 
-            modelBuilder.Entity<UserSubscriptionModel>()
+            builder.Entity<UserSubscriptionModel>()
                 .HasOne(u => u.Subscription)
                 .WithMany()
                 .HasForeignKey(u => u.SubscriptionId)
                 .OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<UserSubscriptionPayment>()
+            builder.Entity<UserSubscriptionPayment>()
                 .HasKey(u => u.Id);
 
-            modelBuilder.Entity<UserSubscriptionPayment>()
+            builder.Entity<UserSubscriptionPayment>()
                 .HasIndex(u => u.UserId)
                 .HasDatabaseName("IX_UserSubscriptionPayments_UserId");
 
-            modelBuilder.Entity<UserSubscriptionPayment>()
+            builder.Entity<UserSubscriptionPayment>()
                 .HasOne(u => u.Subscription)
                 .WithMany()
                 .HasForeignKey(u => u.SubscriptionId)
                 .OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<UserSubscriptionPayment>()
+            builder.Entity<UserSubscriptionPayment>()
                 .HasOne(u => u.Payment)
                 .WithMany()
                 .HasForeignKey(u => u.PaymentId)
                 .OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<AdminRefreshTokenModel>()
+            builder.Entity<AdminRefreshTokenModel>()
                 .HasKey(r => r.Id);
 
-            modelBuilder.Entity<AdminRefreshTokenModel>()
+            builder.Entity<AdminRefreshTokenModel>()
                 .HasOne(r => r.User)
                 .WithMany()
                 .HasForeignKey(r => r.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<AdminRefreshTokenModel>()
+            builder.Entity<AdminRefreshTokenModel>()
                 .HasIndex(r => r.TokenHash);
 
-            modelBuilder.Entity<AdminRefreshTokenModel>()
+            builder.Entity<AdminRefreshTokenModel>()
                 .HasIndex(r => r.UserId);
 
-            modelBuilder.Entity<AdminRefreshTokenModel>()
+            builder.Entity<AdminRefreshTokenModel>()
                 .HasOne(r => r.ReplacedByToken)
                 .WithMany()
                 .HasForeignKey(r => r.ReplacedByTokenId)
